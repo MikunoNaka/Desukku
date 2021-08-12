1f76f783de91fe128efc5d0205ce569ec4efbf05
MikunoNaka <bokuwakanojogahoshii@yahoo.com>
2021-08-12 17:23:47 +0530

    Assining bands, stacks and regions to cells now

--- src/main/java/Desukku/Cell.java
+++ src/main/java/Desukku/Cell.java
@@ -10,15 +10,26 @@ public class Cell extends JToggleButton {
     private final Color bgColorSelected = new Color(0x6E5991);
     private final Color borderColor = new Color(0x000000);
 
-    Cell(String val) {
-        this.setText(val);
+    private final int stackID;
+    private final int bandID;
+    private final int regionID;
+
+    Cell(String val, int stackID, int bandID, int regionID) {
         this.setBackground(bgColor);
         this.setBorder(new LineBorder(borderColor));
         this.setForeground(fgColor);
         this.setFocusable(false);
         this.setFont(new Font("Helvetica", Font.PLAIN, 24));
         super.setContentAreaFilled(false);
+        this.setText(val);
+        this.stackID = stackID;
+        this.bandID = bandID;
+        this.regionID = regionID;
     }
+
+    public int getBand() { return bandID; }
+    public int getStack() { return stackID; }
+    public int getRegion() { return regionID; }
 
     @Override
     protected void paintComponent(Graphics g) {
--- src/main/java/Desukku/Grid.java
+++ src/main/java/Desukku/Grid.java
@@ -16,13 +16,48 @@ public class Grid extends JPanel implements ActionListener {
         this.setBackground(GameWindow.bgColor);
         this.setPreferredSize(new Dimension(0, 450));
         this.setLayout(new GridLayout(9, 9));
-        this.populateControls();
+        this.populateGrid();
     }
 
-    private void populateControls() {
-        // create numeric buttons
-        for (int i = 0; i<81; i++)
-            cells[i] = new Cell("");
+    private void populateGrid() {
+        // create 81 cells and fill values
+        int stackCount = 0;
+        int bandCount = 0;
+
+        for (int i = 0; i<81; i++) {
+            int region = switch(stackCount) {
+                case 0, 1, 2 -> switch(bandCount) {
+                    case 0, 1, 2 -> 0;
+                    case 3, 4, 5 -> 3;
+                    case 6, 7, 8 -> 6;
+                    default -> 9;
+                };
+                case 3, 4, 5 -> switch(bandCount) {
+                    case 0, 1, 2 -> 1;
+                    case 3, 4, 5 -> 4;
+                    case 6, 7, 8 -> 7;
+                    default -> 9;
+                };
+                case 6, 7, 8 -> switch(bandCount) {
+                    case 0, 1, 2 -> 2;
+                    case 3, 4, 5 -> 5;
+                    case 6, 7, 8 -> 8;
+                    default -> 9;
+                };
+                default -> 9;
+            };
+
+            cells[i] = new Cell(String.valueOf(i) /* "" */, stackCount, bandCount, region);
+
+            // set stack and/or band id for next cell
+            stackCount ++;
+            if (stackCount == 9) {
+                stackCount = 0;
+                bandCount++;
+                if (bandCount == 9)
+                    bandCount = 0;
+            }
+        }
 
         // add buttons to panel
         for (Cell cell : cells) {
@@ -37,13 +72,6 @@ public class Grid extends JPanel implements ActionListener {
         for (Cell cell : cells) {
             if (e.getSource() == cell) {
                 cell.setText(controls.getSelectedValue());
-
-                // print bands
-                /*for (String[] i : getBands(cells)) {
-                    for (String j : i)
-                        System.out.print(j + " ");
-                    System.out.println();
-                }*/
 
                 for (String i : getBand(8, cells)) {
                     System.out.println(i);
